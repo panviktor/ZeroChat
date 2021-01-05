f75f9217eba2bcba19441f2766371a2668dd1b40
SwiftViktor <panafviktor@gmail.com>
2021-01-05 13:17:39 +0300

    Initial Commit

---
+++ ZeroChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZeroChat
+//
+//  Created by Viktor on 05.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
