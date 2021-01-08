0b191b01d6d2302c1f199c9b78394eb39ad378b3
SwiftViktor <panafviktor@gmail.com>
2021-01-08 23:02:54 +0300

    add ActiveCell

---
+++ ZeroChat/Controllers/CustomViews/Cell/Protocol+SelfConfiguringCell.swift
@@ -0,0 +1,13 @@
+//
+//  Protocol+SelfConfiguringCell.swift
+//  ZeroChat
+//
+//  Created by Viktor on 08.01.2021.
+//
+
+import Foundation
+
+protocol SelfConfiguringCell {
+    static var reuseId: String { get }
+    func configure(with value: MChat)
+}
